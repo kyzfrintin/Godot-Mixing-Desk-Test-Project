1f0dc777ecb1decea3a62a507b678afe5bdd197b
Kyle Harmieson <kyle.harmo@live.co.uk>
2019-01-12 03:39:13 +0000

    v1.2.1 - added bind function

--- rooms/main.gd
+++ rooms/main.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
 onready var mdm = get_node("MDM")
+onready var point = get_node("point")
 
 var colour : int = 0
 
 func _ready():
 	mdm._init_song(0)
 	mdm._start_alone(0,0)
+	mdm._bind_to_param(0,-10)
+	
+func _process(delta):
+	var dis = $player.position.distance_to(point.position)
+	dis -= 300
+	dis /= 1000
+	if dis < 1:
+		mdm._feed_param(0,dis)
 	
