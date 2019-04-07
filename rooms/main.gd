27f69f98b6ac8f79cc20989d8043d49497f09189
Kyle Harmieson <kyle.harmo@live.co.uk>
2019-04-07 19:30:20 +0100

    1.4 - merged PR #2 and debugged beat lag. Also made balls less bouncy

--- rooms/main.gd
+++ rooms/main.gd
@@ -9,3 +9,12 @@ func _ready():
 	yield(get_tree().create_timer(2),"timeout")
 	mdm._start_alone("orange","epiano")
 	
+
+
+func _on_MDM_beat(beat):
+	print(beat)
+	pass
+
+func _on_MDM_bar(bar):
+#	print('bar ' + str(bar))
+	pass
